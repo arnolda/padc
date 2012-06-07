673e99c915c0e8b841b03412bc5de8f8d8d4c5b0
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2012-06-07 13:00:44 +0200

    Bugfix Bessel-DGL

--- plots/bessel_diff.py
+++ plots/bessel_diff.py
@@ -26,32 +26,32 @@ xmax = 15.0
 ############################################
 
 # Anzahl Punkte Differential
-N = 300
+N = 31
 
 # Schrittweite Differential
-h = xmax/N
+h = xmax/(N-1)
+
+x = arange(0,N)*h
 
 # 3-Punkt-Stencil, Dirichlet-Randbedingung
 ############################################
 
-x3   = arange(1,N+1)*h
-
 # alles erstmal auf Null
-b = numpy.zeros(N)
-A = numpy.zeros((N, N))
+b = zeros(N)
+A = zeros((N, N))
 
 # Startwerte an den Raendern
 A[0, 0] = 1
-b[0]    = jn(nu, h)
-A[1, N -1] = 1
-b[1]    = jn(nu, N*h)
+b[0]    = jn(nu, 0)
+A[1, N - 1] = 1
+b[1]    = jn(nu, (N-1)*h)
 
-# N-2 innere Punkte von 1 bis N-2 hinter den Startwerten, alle auf Null
-for n in range(2, N):
+# N-2 innere Punkte von 1 bis N-2 hinter den Startwerten
+for n in range(1, N-1):
     #             x^2 d^2f/dx^2 + x df/dx    + (x^2 - nu^2)f
-    A[n, n - 2] = 0.5*n**2      - 0.5*n
-    A[n, n - 1] =    -n**2                   + n**2*h**2 - nu**2
-    A[n, n]     = 0.5*n**2      + 0.5*n
+    A[n + 1, n - 1] =     n**2      - 0.5*n
+    A[n + 1, n    ] =  -2*n**2                   + n**2*h**2 - nu**2
+    A[n + 1, n + 1] =     n**2      + 0.5*n
 
 fx3 = solve(A, b)
 
@@ -59,28 +59,31 @@ fx3 = solve(A, b)
 ############################################
 
 # alles erstmal auf Null
-b = numpy.zeros(N)
-A = numpy.zeros((N, N))
+b = zeros(N)
+A = zeros((N, N))
 
-# Startwerte an den Raendern
+# Startwert am Rand
 A[0, 0] = 1
-b[0]    = jn(nu, h)
-A[1, 1] = 1
-b[1]    = jn(nu, 2*h)
+b[0]    = jn(nu, 0)
+# Ableitung
+A[1, 0] = -0.5/h
+A[1, 2] = 0.5/h
+if nu == 0:
+    b[1]    = -jn(1, h)
+else:
+    b[1]    = 0.5*(jn(nu-1)-jn(nu+1))
 
-# N-2 innere Punkte von 1 bis N-2 hinter den Startwerten, alle auf Null
-for n in range(2, N):
+# N-2 innere Punkte von 1 bis N-2
+for n in range(1, N-1):
     #             x^2 d^2f/dx^2 + x df/dx    + (x^2 - nu^2)f
-    A[n, n - 2] = 0.5*n**2      - 0.5*n
-    A[n, n - 1] =    -n**2                   + n**2*h**2 - nu**2
-    A[n, n]     = 0.5*n**2      + 0.5*n
+    A[n + 1, n - 1] =     n**2      - 0.5*n
+    A[n + 1, n    ] =  -2*n**2                   + n**2*h**2 - nu**2
+    A[n + 1, n + 1] =     n**2      + 0.5*n
 
 fx3l = solve(A, b)
 
 # 5-Punkt-Stencil, Dirichlet
 ############################################
-
-x5   = arange(0,N)*h
 
 # alles wieder auf Null
 b = numpy.zeros(N)
@@ -93,11 +96,11 @@ A[1, N - 1] = 1
 b[1]        = jn(nu, (N-1)*h)
 
 # innere naechste 2 Punkte durch einfache Naeherung
-for z,n in ((2,1),(3, N-2)):
+for z,n in ((2, 1), (3, N-2)):
     #             x^2 d^2f/dx^2 + x df/dx    + (x^2 - nu^2)f
-    A[z, n - 1] = 0.5*n**2      - 0.5*n
-    A[z, n    ] =    -n**2                   + n**2*h**2 - nu**2
-    A[z, n + 1] = 0.5*n**2      + 0.5*n
+    A[z, n - 1] =     n**2      - 0.5*n
+    A[z, n    ] =  -2*n**2                   + n**2*h**2 - nu**2
+    A[z, n + 1] =     n**2      + 0.5*n
 
 # N-4 innere Punkte von 2 bis N-3 hinter den Startwerten, alle auf Null
 for n in range(2, N-2):
@@ -115,17 +118,17 @@ fx5 = solve(A, b)
 
 figure = pyplot.figure(figsize=(4,4))
 
-x = linspace(0, xmax, 200)
+xfine = linspace(0, xmax, 200)
 
 # diff equation
 ############################################
 
 graph = figure.add_subplot(111)
 
-graph.plot(x, jn(nu, x), "k-",linewidth=0.5)
-graph.plot(x3, fx3, "g-.")
-graph.plot(x3, fx3l, "r--")
-graph.plot(x5, fx5, "b--", dashes=(1,8), dash_capstyle="round", linewidth=3)
-graph.axis((0, xmax, -0.5, 1))
+graph.plot(xfine, jn(nu, xfine), "k-",linewidth=0.5)
+graph.plot(x, fx3, "D", markersize=2, markerfacecolor="green", markeredgecolor="green")
+graph.plot(x, fx3l, "*", markersize=3, markerfacecolor="red", markeredgecolor="red")
+graph.plot(x, fx5, "o", markersize=2, markerfacecolor="blue", markeredgecolor="blue")
+graph.axis((0, xmax, -0.6, 1.4))
 
 figure.savefig("bessel_diff.pdf")
